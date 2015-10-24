d0fa09a3d89f70dbd20089a5ae321624a8d444fd
h-yamashita <h-yamashita@tes.co.jp>
2015-10-24 12:12:47 +0900

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,10 @@ class Micropost < ActiveRecord::Base
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+
+  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
+  end
 end
